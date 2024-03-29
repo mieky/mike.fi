1767cd0970991740ba46bf40b3d7b1fd276b7aa4
Mike Arvela <mike@arvela.net>
2024-03-29 14:03:46 +0200

    Tweaky tweakety tweak

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,29 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // main: "#6161ff",
+        // main: "#8599f7",
+        // main: "#a07ff8",
+        // main: "#ebc6ca",
+        main: "#aa5561",
+        zinc: {
+          80: "#f7f7f7",
+          450: "#85858f",
+        },
+      },
+      fontSize: {
+        "code-sm": "0.825rem",
+        "code-base": "0.925rem",
+        "code-lg": "1.12rem",
+        "code-xl": "1.2rem",
+        "code-2xl": "1.45rem",
+        "code-3xl": "1.825rem",
+        "code-4xl": "2.15rem",
+        "code-5xl": "2.9rem",
+      },
+    },
   },
   plugins: [],
 }
