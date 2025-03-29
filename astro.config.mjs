0c0e79ec147bcced26a74dab7cff34a414a735d3
Mike Arvela <mike@arvela.net>
2025-03-29 10:11:24 +0200

    Update to Tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
 import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
-import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mike.fi",
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [mdx(), sitemap()],
+
   markdown: {
     shikiConfig: {
       // https://shiki.style/themes
       theme: "tokyo-night",
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 })
